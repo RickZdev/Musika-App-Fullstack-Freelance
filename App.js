1c32b9f21a15bb058665a2dd408e9647562a6abc
Fred <frederick.castanedajr04@gmail.com>
2022-11-29 04:22:40 +0800

    initial project

---
+++ App.js
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>HAHA</Text>
+    </View>
+  );
+};
+
+export default App;
